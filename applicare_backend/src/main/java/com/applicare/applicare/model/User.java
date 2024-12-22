dba3eb806473a4d5e069ab16f8573bab35d3e7a0
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2024-12-22 15:00:54 +0100

    fixed an issue that allowed users register with the same name and email, its fixed now

--- applicare_backend/src/main/java/com/applicare/applicare/model/User.java
+++ applicare_backend/src/main/java/com/applicare/applicare/model/User.java
@@ -3,6 +3,7 @@
 package com.applicare.applicare.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -11,43 +12,43 @@ public class User {
     @Id
     private String id;
 
+    @Indexed(unique = true)
     private String username;
+
+    @Indexed(unique = true)
     private String email;
+
     private String password;
     private String passwordResetToken;
 
     public User() {}
+    
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    // getters & setters
     public String getId() {
         return id;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
     public String getEmail() {
         return email;
-    }
-    
+    } 
     public void setEmail(String email) {
         this.email = email;
     }
 
     public String getPassword() {
         return password;
-    }
-    
+    } 
     public void setPassword(String password) {
         this.password = password;
     }
@@ -55,7 +56,6 @@ public class User {
     public String getPasswordResetToken() {
         return passwordResetToken;
     }
-
     public void setPasswordResetToken(String passwordResetToken) {
         this.passwordResetToken = passwordResetToken;
     }
--- applicare_backend/src/main/java/com/applicare/applicare/service/AuthService.java
+++ applicare_backend/src/main/java/com/applicare/applicare/service/AuthService.java
@@ -1,11 +1,10 @@
-// com/applicare/applicare/service/AuthService.java
-
 package com.applicare.applicare.service;
 
 import com.applicare.applicare.model.User;
 import com.applicare.applicare.repository.UserRepository;
 import com.applicare.applicare.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,7 +25,7 @@ public class AuthService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
+
     // LOGIN
     public String login(String username, String rawPassword) {
         Optional<User> opt = userRepository.findByUsername(username);
@@ -35,21 +34,33 @@ public class AuthService {
         }
 
         User user = opt.get();
-        // compare hashed
         if (!passwordEncoder.matches(rawPassword, user.getPassword())) {
             throw new RuntimeException("Invalid username or password");
         }
 
-        // generate JWT
         return jwtUtil.generateToken(username);
     }
-    
+
     // REGISTER
     public String register(String username, String email, String rawPassword) {
-        String hashed = passwordEncoder.encode(rawPassword);
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("Username already in use");
+        }
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new RuntimeException("Email already in use");
+        }
 
+        String hashed = passwordEncoder.encode(rawPassword);
         User newUser = new User(username, email, hashed);
-        userRepository.save(newUser);
+
+        try {
+            userRepository.save(newUser);
+        } catch (DuplicateKeyException ex) {
+            throw new RuntimeException(
+                "Username or Email is already in use. Please choose a different one."
+            );
+        }
+
         return "User registered successfully";
     }
 
@@ -69,7 +80,7 @@ public class AuthService {
 
         return "A reset link has been emailed to " + email;
     }
-    
+
     // RESET PASSWORD
     public String resetPassword(String token, String rawPassword) {
         Optional<User> opt = userRepository.findByPasswordResetToken(token);

08a452d2ea4ebc877513b743684d682de68c86e0
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2024-12-22 13:31:24 +0100

    no longer using hardcoded secret

--- applicare_backend/src/main/java/com/applicare/applicare/util/JwtUtil.java
+++ applicare_backend/src/main/java/com/applicare/applicare/util/JwtUtil.java
@@ -5,21 +5,26 @@ package com.applicare.applicare.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.Base64;
 
 @Component
 public class JwtUtil {
 
-    // WILL CHANGE IN PRODUCTION
-    private static final String SECRET = "skLCJ28GHA9pRd6X9SKeLfjZ3PrdJ0ta";
-
-    private final Key signingKey = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    // reads JWT secret from .env
+    private final Key signingKey; // openssl rand -base64 number_of_bytes
 
     private final long EXPIRATION_MS = 3600000; // 1 hour
+
+    public JwtUtil(@Value("${JWT_SECRET}") String secret) {
+        // decodes the secret from base64
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        this.signingKey = Keys.hmacShaKeyFor(decodedKey);
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
@@ -38,7 +43,7 @@ public class JwtUtil {
                 .parseClaimsJws(token);
             return true;
         } catch (Exception e) {
-            return false;
+            return false; // if the token is invalid or expired
         }
     }
 
